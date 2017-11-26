d0c5b71818c87345df7e436de88db2ba912410a2
Michael Pietsch <michael@mi-pietsch.de>
2017-11-26 13:33:24 +0100

    Config on install start + Bumb MS version + MS unattended install + fixes

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Bash Menu Script Example
 InstallFilesFolder=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-MailScannerVersion="5.0.6-4"
+MailScannerVersion="5.0.6-5"
 
 TmpDir="/tmp/mailwatchinstall"
 #MailWatchVersion="v1.2.6"
@@ -18,8 +18,12 @@ source "$InstallFilesFolder/setup.scripts/mailscanner/mailwatch-mailscanner.inc"
 source "$InstallFilesFolder/setup.scripts/mysql/mailwatch-mysql.inc"
 source "$InstallFilesFolder/setup.scripts/php/mailwatch-php.inc"
 
-logprint "Clearing temp dir $TmpDir"
-rm -rf /tmp/mailwatchinstall/*
+if ! [ -d $TmpDir ]; then
+    logprint "Warning: temporary directory from previous install found."
+    logprint "This usally means that a previous install is still running or was interrupted."
+    logprint "In case of an upgrade you should make sure that your old conf.php and skin.css are still"
+    logprint "in the web folder or backup the ones from the temporary folder $TmpDir before continuing."
+fi
 
 detectos
 
@@ -35,27 +39,28 @@ if ! ( type "tar" > /dev/null 2>&1 ) ; then
     logprint ""
 fi
 
-logprint ""
 if [[ -z $(grep mtagroup /etc/group) ]]; then
     logprint "Creating group mtagroup"
     groupadd mtagroup
 fi
 
-download-mailwatch
-
-install-mailscanner
-
 ##ask directory for web files
-logprint ""
 ask "In what location should MailWatch be installed (web files directory)?[/opt/mailwatch/public/]:" WebFolder
 if [ -z $WebFolder ]; then
     WebFolder="/opt/mailwatch/public/"
 fi
 logprint "Using web directory $WebFolder"
 
-install-mailwatch
+check-and-prepare-update
 
+#also detects MTA
+prepare-mailscanner
+prepare-webserver
 configure-sqlcredentials
+
+
+install-mailscanner
+install-mailwatch
 
 install-mysql
 
@@ -63,89 +68,50 @@ configure-mysql
 
 ######################Configure web server ########################
 logprint ""
-if ( type "httpd" > /dev/null 2>&1 ) || ( type "apache2" > /dev/null 2>&1 ); then
-    WebServer="apache"
-    logprint "Detected installed web server apache. We will use it for MailWatch"
-elif ( type "nginx" > /dev/null 2>&1 ); then
-    WebServer="nginx"
-    logprint "Detected installed web server nginx. We will use it for MailWatch"
-else
-    echo "We're unable to find your webserver.  We support Apache and Nginx";echo;
-    echo "Do you wish to install a webserver?"
-    echo "1 - Apache"
-    echo "2 - Nginx"
-    echo "n - do not install or configure"
-    echo;
-    ask "Select Webserver: " webserverSelect
-    if [[ $webserverSelect =~ ^([nN][oO])$ ]]; then
-        #do not install or configure webserver
-        WebServer="skip"
-    elif [ $webserverSelect == 1 ]; then
-        #Apache
-        logprint "Installing apache"
-        if [ $PM == "yum" ]; then
-            $PM install httpd
-        else
-            $PM install apache2
-        fi
-        WebServer="apache"
-    elif [ $webserverSelect == 2 ]; then
-        #Nginx
-        logprint "Installing nginx"
-        $PM install nginx
-        WebServer="nginx"
+if [ "$WebServer" == "apache" ]; then
+    #Apache
+    logprint "Installing apache"
+    if [ $PM == "yum" ]; then
+        $PM install httpd
     else
-        WebServer="skip"
+        $PM install apache2
     fi
+    logprint "Creating config for apache"
+    "$InstallFilesFolder/setup.scripts/apache/mailwatch-apache.sh" "$WebFolder"
+    if [[ "$PM" == "yum"* ]]; then
+        Webuser="apache"
+    else
+        Webuser="www-data"
+    fi
+elif [ "$WebServer" == "nginx" ]; then
+    #Nginx
+    logprint "Installing nginx"
+    $PM install nginx
+    if [[ "$PM" == "yum"* ]]; then
+        Webuser="nginx"
+    else
+        Webuser="www-data"
+    fi
+    TODO
+    logprint "Nginx automatic configuration not available yet"
+else
+    logprint "Skipping web server install"
+    EndNotice="$EndNotice \n * you need to configure your webserver for directory $WebFolder."
+    ask "MailWatch needs to assign permissions to the webserver user. What user is your webserver running at?: " Webuser
 fi
 
 install-php
 
-# configure webserver
-logprint ""
-case $WebServer in
-#TODO::Check compatibility with RPM version
-    "apache")
-        logprint "Creating config for apache"
-        "$InstallFilesFolder/setup.scripts/apache/mailwatch-apache.sh" "$WebFolder"
-        if [[ "$PM" == "yum"* ]]; then
-            Webuser="apache"
-        else
-            Webuser="www-data"
-        fi
-        sleep 1
-        ;;
-    "nginx")
-       #TODO
-        if [[ "$PM" == "yum"* ]]; then
-            Webuser="nginx"
-        else
-            Webuser="www-data"
-        fi
-        logprint "Nginx automatic configuration not available yet"
-        sleep 1
-        ;;
-    "skip")
-        logprint "Skipping web server install"
-        EndNotice="$EndNotice \n * you need to configure your webserver for directory $WebFolder."
-        ask "MailWatch needs to assign permissions to the webserver user. What user is your webserver running at?: " Webuser
-        sleep 1
-        ;;
-esac
-
 configure-mailwatch
 
 #####################apply adjustments for MTAs ########################
-logprint 'Which MTA do you want to use with MailWatch? (it should already be installed):'
-ask 'MTAs: 1:postfix, 2:exim, 3:sendmail, 4/n: skip ' useMta
-logprint "Selected mta $useMta"
-if [ "$useMta" == "1" ]; then
+if [ "$MTA" == "postfix" ]; then
     logprint "Configure MailScanner for use with postfix"
     "$InstallFilesFolder/setup.scripts/postfix/mailwatch-postfix.sh" "$Webuser"
-elif [ "$useMta" == "2" ];then
+elif [ "$MTA" == "exim" ];then
     logprint "Configure MailScanner for use with exim"
     "$InstallFilesFolder/setup.scripts/exim/mailwatch-exim.sh" "$Webuser"
-elif [ "$useMta" == "3" ]; then
+elif [ "$MTA" == "sendmail" ]; then
     logprint "Sendmail is not yet supported. If you have experience with sendmail and want to help support this please contact us at https://github.com/mailwatch/mailwatch-install-script"
     #TODO
 else
@@ -159,6 +125,11 @@ configure-sudo
 
 #todo relay files
 logprint "Install finished!"
+
+logprint "Cleanup temporary data"
+rm -rf /tmp/mailwatchinstall/*
+resetVariables
+
 logprint ""
 logprint "Next steps you have to do are:"
 logprint "$EndNotice"
@@ -169,3 +140,4 @@ resetVariables
 
 echo ""
 echo "You can find the log file at /root/mailwatchInstall.log"
+
