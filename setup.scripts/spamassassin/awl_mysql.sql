d27aaa0dbfb82b043f5bab2f9dc4609638cc470a
stefaweb <sleclerc@actionweb.fr>
2018-07-16 16:22:00 +0200

    Revert "Add bayes schema for mysql"
    This reverts commit a5ab52ede701f1cda4c42c0d1a8347f82278d1db.

--- setup.scripts/spamassassin/awl_mysql.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE awl (
-  username varchar(100) NOT NULL default '',
-  email varbinary(255) NOT NULL default '',
-  ip varchar(40) NOT NULL default '',
-  count int(11) NOT NULL default '0',
-  totscore float NOT NULL default '0',
-  signedby varchar(255) NOT NULL default '',
-  PRIMARY KEY (username,email,signedby,ip)
-) ENGINE=MyISAM;
