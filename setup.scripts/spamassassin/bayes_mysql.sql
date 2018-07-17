073f01d1cdc4feeaf28a5bad0d943b3cbb4f8b65
Michael Pietsch <michael@mi-pietsch.de>
2018-07-17 10:57:07 +0200

    fix typo on installed bayes sql detection+fix bayes sql+fix invalid sql pass on bayes install

--- setup.scripts/spamassassin/bayes_mysql.sql
+++ setup.scripts/spamassassin/bayes_mysql.sql
@@ -1,19 +1,22 @@
 
 CREATE TABLE IF NOT EXISTS `bayes_expire` (
   `id` int(11) NOT NULL DEFAULT '0',
-  `runtime` int(11) NOT NULL DEFAULT '0'
+  `runtime` int(11) NOT NULL DEFAULT '0',
+  KEY `bayes_expire_idx1` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `bayes_global_vars` (
   `variable` varchar(30) NOT NULL DEFAULT '',
-  `value` varchar(200) NOT NULL DEFAULT ''
+  `value` varchar(200) NOT NULL DEFAULT '',
+  PRIMARY KEY (`variable`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `bayes_seen` (
   `id` int(11) NOT NULL DEFAULT '0',
   `msgid` varchar(200) NOT NULL DEFAULT '',
   `flag` char(1) NOT NULL DEFAULT '',
-  `lastupdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `lastupdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`,`msgid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `bayes_token` (
@@ -21,7 +24,9 @@ CREATE TABLE IF NOT EXISTS `bayes_token` (
   `token` binary(5) NOT NULL DEFAULT '',
   `spam_count` int(11) NOT NULL DEFAULT '0',
   `ham_count` int(11) NOT NULL DEFAULT '0',
-  `atime` int(11) NOT NULL DEFAULT '0'
+  `atime` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`,`token`),
+  KEY `bayes_token_idx1` (`id`,`atime`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `bayes_vars` (
@@ -34,25 +39,9 @@ CREATE TABLE IF NOT EXISTS `bayes_vars` (
   `last_atime_delta` int(11) NOT NULL DEFAULT '0',
   `last_expire_reduce` int(11) NOT NULL DEFAULT '0',
   `oldest_token_age` int(11) NOT NULL DEFAULT '2147483647',
-  `newest_token_age` int(11) NOT NULL DEFAULT '0'
+  `newest_token_age` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `bayes_vars_idx1` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE `bayes_expire`
- ADD KEY `bayes_expire_idx1` (`id`);
-
-ALTER TABLE `bayes_global_vars`
- ADD PRIMARY KEY (`variable`);
-
-ALTER TABLE `bayes_seen`
- ADD PRIMARY KEY (`id`,`msgid`);
-
-ALTER TABLE `bayes_token`
- ADD PRIMARY KEY (`id`,`token`), ADD KEY `bayes_token_idx1` (`id`,`atime`);
-
-ALTER TABLE `bayes_vars`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `bayes_vars_idx1` (`username`);
-
-ALTER TABLE `bayes_vars`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-INSERT INTO bayes_global_vars VALUES ('VERSION','3');
+REPLACE INTO bayes_global_vars VALUES ('VERSION','3');
